93c7935539835047f895b14681ac1ef287522ff9
Teddy <tdurie24@gmail.com>
2020-10-21 03:35:33 +0200

    website

---
+++ Repos/EbenWebsite/EbenWeb/Controllers/ServicesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EbenWeb.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PropertyInvestments()
+        {
+            return View();
+        }
+
+        public IActionResult RentalManagement()
+        {
+            return View();
+        }
+
+        public IActionResult PropertyMaintenance()
+        {
+            return View();
+        }
+
+        public IActionResult Architecture()
+        {
+            return View();
+        }
+    }
+}
